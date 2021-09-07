8a3804b55da3725d376562d65c7713944319bd1a
jmg292 <jeff@gnzlabs.io>
2021-09-07 10:16:39 -0500

    Added missing hardware modules

---
+++ usb_controller/data_types/__init__.py
@@ -0,0 +1,3 @@
+from .current_mode import CurrentMode
+
+__all__ = ["CurrentMode"]
---
+++ usb_controller/data_types/current_mode.py
@@ -0,0 +1,9 @@
+from enum import IntEnum
+
+
+class CurrentMode(IntEnum):
+
+    HalfAmp = 0x00
+    OneAmp = 0x01
+    HalfAmp = 0x02
+    ThreeAmp = 0x03
